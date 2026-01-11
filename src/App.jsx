d19205d0953f809b561543b049faa2650dc5d06a
John Dio Lumacang <tcshs.lumacang.johndio.a@gmail.com>
2026-01-12 07:15:11 +0800

    added copyToClipbpard function

--- src/App.jsx
+++ src/App.jsx
@@ -116,6 +116,15 @@ function App() {
     }
   }
 
+  async function copyToClipboard(text) {
+    try {
+      await navigator.clipboard.writeText(text);
+      alert("Copied to clipboard!");
+    } catch (err) {
+      console.error("Failed to copy!", err);
+    }
+  }
+
   // otherwise, show the login form
   return (
     <div className="container">
